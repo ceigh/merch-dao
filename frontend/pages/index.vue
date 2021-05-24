05ab1c023b4c5da8cdca38c89636005c267e3f6e
Artjom Löbsack <me@ceigh.com>
2021-05-24 18:42:41 +0500

    feat(frontend): add homepage template

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>index</div>
+  <div>
+    <p>{{ item.description }}</p>
+
+    <b-button variant="primary">
+      Buy now
+    </b-button>
+
+    <b-img :src="item.images[0]" alt="image" />
+
+    <div>
+      <p>{{ quantityFormatted }}</p>
+      <p v-if="item.quantity !== -1">
+        pcs only Limited
+      </p>
+    </div>
+  </div>
 </template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import type { Item } from '../../types'
+
+export default Vue.extend({
+  async fetch ({ app: { $accessor } }) {
+    if ($accessor.options.currentItem === '') {
+      throw new Error('no current item')
+    } else { await $accessor.items.getCurrent() }
+  },
+
+  computed: {
+    item (): Item {
+      return this.$accessor.items.current
+    },
+    quantityFormatted (): string {
+      const { quantity } = this.item
+      return quantity === -1 ? 'Unlimited' : `${quantity}X`
+    }
+  }
+})
+</script>
--- frontend/store/items.ts
+++ frontend/store/items.ts
@@ -3,12 +3,16 @@ import type { Options, Item } from '../../types'
 import type { Create, Update, Delete } from '../../types/api/items'
 
 export const state = () => ({
-  all: [] as Item[]
+  all: [] as Item[],
+  current: {} as Item
 })
 
 export const mutations = mutationTree(state, {
   setAll (state, all: Item[]) {
     state.all = all
+  },
+  setCurrent (state, item: Item) {
+    state.current = item
   }
 })
 
@@ -33,6 +37,13 @@ export const actions = actionTree({ state, mutations }, {
   async getAll (): Promise<void> {
     const { data: { items } } = await this.$axios.get('/items')
     this.app.$accessor.items.setAll(items)
+  },
+
+  async getCurrent (): Promise<void> {
+    const { $accessor } = this.app
+    const id = $accessor.options.currentItem
+    const { data } = await this.$axios.get(`/item/${id}`)
+    $accessor.items.setCurrent(data)
   },
 
   async updateCurrent (_, id: Options['currentItem']): Promise<void> {
